b778860e0d5b7365f62c701bb508e00011df1789
Isabella <isabellarose.hernandez01@gmail.com>
2022-03-24 13:46:19 -0500

    finished subqueries exercises, not bonus

---
+++ subqueries_exercises.sql
@@ -0,0 +1,28 @@
+USE employees;
+
+-- Find all the employees with the same hire date as employee 101010 using a subquery.
+-- SELECT *
+-- FROM employees
+-- WHERE hire_date IN (
+--     SELECT hire_date
+--     FROM employees
+--     WHERE emp_no = 101010
+--     );
+
+-- Find all the titles held by all employees with the first name Aamod.
+-- SELECT title
+-- FROM titles
+-- WHERE emp_no IN (
+--     SELECT emp_no
+--     FROM employees
+--     WHERE first_name = 'Aamod'
+--     );
+
+-- Find all the current department managers that are female.
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE emp_no IN (
+--     SELECT emp_no
+--     FROM dept_manager
+--     WHERE gender = 'F' AND YEAR(to_date) = 9999
+--     );
